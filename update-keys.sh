358f0fedb65401b0367c887a9daff74a2794f649
Christopher Horrell <christopher@horrell.ca>
2024-06-05 21:29:47 -0400

    Select correct field and remove any dupes

--- update-keys.sh
+++ update-keys.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -ex
 
-curl -fsSLo- --compressed https://github.com/nodejs/node/raw/main/README.md | awk '/^gpg --keyserver hkps:\/\/keys\.openpgp\.org --recv-keys/ {print $NF}' > keys/node.keys
+curl -fsSLo- --compressed https://github.com/nodejs/node/raw/main/README.md | awk '/^gpg --keyserver hkps:\/\/keys\.openpgp\.org --recv-keys/ {print $5}' | uniq > keys/node.keys
